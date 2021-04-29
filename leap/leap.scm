0140db54fb75bbff7f076dbf5217ad93befeba2b
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-04-29 14:03:51 +0200

    Solve exercise "Leap"

--- leap/leap.scm
+++ leap/leap.scm
@@ -1,5 +1,20 @@
 (import (rnrs))
 
+(define (div? a b)
+  (zero? (modulo a b))
+  )
+
+(define (!div? a b)
+  (not (div? a b))
+  )
+
 (define (leap-year? year)
-  'implement-me!)
+  (and
+   (div? year 4)
+   (or
+    (!div? year 100)
+    (div? year 400)
+    )
+   )
+  )
 
