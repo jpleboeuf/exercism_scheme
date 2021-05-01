9d886cf62c29064e83a5bcbebe12079b348161d2
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-05-01 20:44:15 +0200

    Solve exercise "RNA Transcription"

--- rna-transcription/rna-transcription.scm
+++ rna-transcription/rna-transcription.scm
@@ -1,5 +1,16 @@
 (import (rnrs))
 
+(define nucleotideTransTbl (make-eq-hashtable))
+(eq-hashtable-set! nucleotideTransTbl #\G #\C)
+(eq-hashtable-set! nucleotideTransTbl #\C #\G)
+(eq-hashtable-set! nucleotideTransTbl #\T #\A)
+(eq-hashtable-set! nucleotideTransTbl #\A #\U)
+
 (define (dna->rna dna)
-  'implement-me!)
+  (list->string 
+   (map (lambda (n) (eq-hashtable-ref nucleotideTransTbl n #\?))
+        (string->list dna)
+        )
+   )
+  )
 
